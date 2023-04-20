3be18737a18695e2d725add06f8bef0ec1d3cc43
wjdwodhks35 <88700168+leeda06@users.noreply.github.com>
2023-04-20 20:07:09 +0900

    안드로이드 4월 20일

---
+++ app/src/main/java/com/example/kotlin_android_study/ViewStudyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_android_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStudyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_study)
+    }
+}
