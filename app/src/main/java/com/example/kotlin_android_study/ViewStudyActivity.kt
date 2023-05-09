280f36b30bbf15a9bb1dc3b66d174219e2ce7e11
wjdwodhks35 <88700168+leeda06@users.noreply.github.com>
2023-05-09 20:02:22 +0900

    안드로이드 5월 09일

--- app/src/main/java/com/example/kotlin_android_study/ViewStudyActivity.kt
+++ app/src/main/java/com/example/kotlin_android_study/ViewStudyActivity.kt
@@ -2,10 +2,35 @@ package com.example.kotlin_android_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class ViewStudyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_study)
+        setContentView(R.layout.view_study2)
+
+        var spinner = findViewById<Spinner>(R.id.my_spinner)
+        val menuItems = arrayOf("Item 1", "Item 2", "Item 3")
+        val adapter: ArrayAdapter<String?> = ArrayAdapter<String?>(
+            this,
+            android.R.layout.simple_spinner_item,
+            menuItems
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        // spinner.setAdapter(adapter)
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override  fun onItemSelected(p0: AdapterView<*>?, p1: View?, poisition: Int, id: Long){
+                val selectedItem = p0?.getItemAtPosition(poisition).toString()
+                Log.d("mytag", selectedItem)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
     }
 }
